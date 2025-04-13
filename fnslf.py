5c63c3e8d6b81242aea12f528aed8587c8616ae7
Solevaral <dmitrienkosv5@gmail.com>
2025-04-13 12:25:22 +0300

    upd 7

--- fnslf.py
+++ fnslf.py
@@ -4,7 +4,7 @@ import fnmatch
 from datetime import datetime
 
 # Определяем системные директории для исключения
-SYSTEM_DIRS = {'/proc', '/sys', '/dev', '/run', '/tmp', '/snap', '/usr', '/var', '/.var', '/etc', '/root'}
+SYSTEM_DIRS = {'/proc', '/sys', '/dev', '/run', '/tmp', '/snap', '/usr', '/var', '/etc', '/root'}
 
 def is_hidden(filepath):
     return os.path.basename(filepath).startswith('.') and not filepath.endswith(('/', '/.', '/..'))
